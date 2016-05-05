8f30aad8f5eebdd53eec9d6d9887da5f6f226467
olexandr.spivak <olexandr.spivak@lemberg.co.uk>
2016-05-05 13:11:21 +0300

    fix app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 // TODO Create a passport auth
-//
+// TODO registration
+// TODO frontend side
 
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
+//var path = require('path');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+//var routes = require('./routes/index');
 var users = require('./routes/users');
 var categories = require('./routes/categories');
 var posts = require('./routes/posts');
@@ -16,8 +17,8 @@ var posts = require('./routes/posts');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+///app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,9 +26,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+//app.use('/', routes);
 //app.use('/users', users);
 app.use(users);
 app.use(categories);
